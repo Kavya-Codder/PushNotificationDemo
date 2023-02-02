1d678ba84a279440c3fc8f9a8cad99aa7e2cf35e
Kavya Prajapati <kavya8958p@gmail.com>
2023-02-02 22:57:01 +0530

    Push notification configuration

---
+++ Pods/Target Support Files/Pods-PushNotification(Demo)-PushNotification(Demo)UITests/Pods-PushNotification(Demo)-PushNotification(Demo)UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PushNotification_Demo__PushNotification_Demo_UITests : NSObject
+@end
+@implementation PodsDummy_Pods_PushNotification_Demo__PushNotification_Demo_UITests
+@end
---
+++ PushNotification(Demo)/AppDelegate.swift
@@ -0,0 +1,86 @@
+//
+//  AppDelegate.swift
+//  PushNotification(Demo)
+//
+//  Created by Sunil Developer on 02/02/23.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseMessaging
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        configureAndRegisterForPushNotifications()
+        return true
+    }
+    
+    func configureAndRegisterForPushNotifications() {
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { (granted, error) in
+            let nc = NotificationCenter.default
+            nc.post(name: Notification.Name("AllowNotification"), object: granted)
+            guard granted else {
+                print("User Denied the permission to receive Push")
+                print("Error \(error?.localizedDescription ?? "")")
+                return
+            }
+        }
+        UNUserNotificationCenter.current().delegate = self
+        UIApplication.shared.registerForRemoteNotifications()
+     
+        Messaging.messaging().delegate = self
+    }
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+    }
+}
+
+extension AppDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print(notification.request.content)
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+         //getting error on registering notification
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print(userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+}
+
